1a3860c5dc61f2ea3d6668c60a92efd44f262ee5
okmttdhr <10101114fin@gmail.com>
2015-08-28 20:52:06 +0900

    テンプレートダミーのデータ

--- app/stores/TemplateStore.js
+++ app/stores/TemplateStore.js
@@ -2,6 +2,39 @@ import AppDispatcher from '../dispatcher/AppDispatcher'
 import TemplateConstants from '../constants/TemplateConstants'
 import createStore from '../utils/createStore'
 
+var _templates = [
+  {
+    id: 1,
+    name: 'チケット',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'チケット(Redmine)',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 3,
+    name: 'プルリク',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'プルリク(Redmine)',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 5,
+    name: 'プルリク(レビュー)',
+    content: '~~~~~~~~',
+    selected: false
+  }
+];
+
 export default createStore({
 
 })
