a3ea924f303809dc1cc6f110efc7f085959f64d1
okmttdhr <10101114fin@gmail.com>
2015-08-28 20:58:07 +0900

    database

---
+++ app/utils/TemplatesData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'チケット',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'チケット(Redmine)',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 3,
+    name: 'プルリク',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'プルリク(Redmine)',
+    content: '~~~~~~~~',
+    selected: false
+  },
+  {
+    id: 5,
+    name: 'プルリク(レビュー)',
+    content: '~~~~~~~~',
+    selected: false
+  }
+]
